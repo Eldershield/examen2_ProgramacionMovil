73fab58ab4e3aa09151eb5d863975d99800cf8b7
Jesus Bautista <107812965+Eldershield@users.noreply.github.com>
2023-12-16 08:48:18 -0600

    tercer commit con la creacion del servicio en book_service.dart

---
+++ lib/services/book_service.dart
@@ -0,0 +1,16 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_books_api/Models/book.dart';
+
+class BookService {
+  final Dio _dio = Dio();
+  final String _baseUrl = 'https://stephen-king-api.onrender.com';
+
+  Future<Iterable<Book>> getBooks() async {
+    try {
+      final response = await _dio.get('$_baseUrl/books');
+      return (response.data as List).map((book) => Book.fromJson(book));
+    } catch (e) {
+      throw Exception('Error fetching books: $e');
+    }
+  }
+}
