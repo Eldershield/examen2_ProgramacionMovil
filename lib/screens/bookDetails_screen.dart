5c7eaead9dc28aea163e282db12368f6a46c99fb
Jesus Bautista <107812965+Eldershield@users.noreply.github.com>
2023-12-16 10:44:46 -0600

    Sexto commit con arreglos en el codigo, finalmente muestra la info

--- lib/screens/bookDetails_screen.dart
+++ lib/screens/bookDetails_screen.dart
@@ -10,13 +10,39 @@ class bookDetailScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: Text(book.title)),
       body: Padding(
-        padding: EdgeInsets.all(8.0),
+        padding: const EdgeInsets.all(8.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(book.description),
-            SizedBox(height: 10),
-            Text('Villains: ${book.villains.join(', ')}'),
+            Text(
+              'Title: ${book.title}',
+              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            Text(
+              'ID: ${book.id}',
+              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            const Text(
+              'Description: ',
+              style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            Text(
+              '${book.description}',
+              style: const TextStyle(
+                fontSize: 16,
+              ),
+            ),
+            const SizedBox(height: 10),
+            const Text(
+              'Villains:',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: book.villains
+                  .map((villain) => Text(villain['name'] ?? 'Unknown'))
+                  .toList(),
+            ),
           ],
         ),
       ),
--- lib/services/book_service.dart
+++ lib/services/book_service.dart
@@ -7,11 +7,31 @@ class BookService {
 
   Future<List<Book>> getBooks() async {
     try {
-      final response = await _dio.get('$_baseUrl/books');
-      return (response.data as List)
-          .map((book) => Book.fromJson(book))
-          .toList();
+      final response = await _dio.get(
+        '$_baseUrl/api/books',
+        options: Options(
+          validateStatus: (status) {
+            return status! < 500;
+          },
+        ),
+      );
+
+      print('Response status code: ${response.statusCode}');
+      print('Response data: ${response.data}');
+
+      if (response.statusCode == 200) {
+        final List<dynamic> bookDataList = response.data['data'];
+
+        if (bookDataList.isNotEmpty) {
+          return bookDataList.map((book) => Book.fromJson(book)).toList();
+        } else {
+          throw Exception('No books found in the response data');
+        }
+      } else {
+        throw Exception('Failed to load books: ${response.statusCode}');
+      }
     } catch (e) {
+      print('Error fetching books: $e');
       throw Exception('Error fetching books: $e');
     }
   }
